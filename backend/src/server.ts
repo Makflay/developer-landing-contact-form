b5aa6442257cc8c78202027ccc482d9d28c59c21
Viktor (Makflay) <chief.makflay1@gmail.com>
2026-05-22 15:52:10 +0300

    feat: add basic Express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,23 @@
-console.log("Backend started");
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config({ path: "../.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Backend is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
